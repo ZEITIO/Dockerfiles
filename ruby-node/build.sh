b474ece7e5fd8bada0424a4d0752204e604a051f
Marian Theisen <marian@cice-online.net>
2019-12-13 10:37:19 +0100

    use cli args for versions

--- ruby-node/build.sh
+++ ruby-node/build.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-export RUBY_VERSION=2.6.5
-export NODE_VERSION=10
+export RUBY_VERSION=$1
+export NODE_VERSION=$2
 
 docker build . -t zeitio/ruby-node-stack:ruby${RUBY_VERSION}_node${NODE_VERSION} --build-arg RUBY_VERSION=${RUBY_VERSION} --build-arg NODE_VERSION=${NODE_VERSION}
 
