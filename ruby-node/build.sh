8264c86181ea06b22ebe0830ae4a9d6ba8ae3ca8
Marian Theisen <marian@cice-online.net>
2019-12-13 09:36:44 +0100

    update images

--- ruby-node/build.sh
+++ ruby-node/build.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
-docker build . -t zeitio/ruby-node-stack:ruby2.6.3_node10
+export RUBY_VERSION=2.6.5
+export NODE_VERSION=10
 
-docker push zeitio/ruby-node-stack:ruby2.6.3_node10
+docker build . -t zeitio/ruby-node-stack:ruby${RUBY_VERSION}_node${NODE_VERSION} --build-arg RUBY_VERSION=${RUBY_VERSION} --build-arg NODE_VERSION=${NODE_VERSION}
+
+docker push zeitio/ruby-node-stack:ruby${RUBY_VERSION}_node${NODE_VERSION}
 
