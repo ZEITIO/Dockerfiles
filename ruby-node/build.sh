287f1338e3a693c480bf37079c54ca6d90ad52c9
Marian Theisen <marian@cice-online.net>
2020-05-04 09:24:18 +0200

    updates

--- ruby-node/build.sh
+++ ruby-node/build.sh
@@ -6,4 +6,3 @@ export NODE_VERSION=$2
 docker build . -t zeitio/ruby-node-stack:ruby${RUBY_VERSION}_node${NODE_VERSION} --build-arg RUBY_VERSION=${RUBY_VERSION} --build-arg NODE_VERSION=${NODE_VERSION}
 
 docker push zeitio/ruby-node-stack:ruby${RUBY_VERSION}_node${NODE_VERSION}
-
